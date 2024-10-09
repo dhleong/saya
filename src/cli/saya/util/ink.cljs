5fed55f58cf783793001d507fc97b164e328a861
Daniel Leong <me@dhleong.net>
2024-10-08 20:34:23 -0400

    Hack in a custom renderer (#1)
    * Scaffold a custom renderer
    
    We're abusing the fact that the current version of ink will
    always just call stdout.write with a clearTerminal + the full
    screen of text. The idea here is we can diff the last output and
    the new output to improve performance
    
    * Fill out a line-diffing renderer
    
    This is not *maximally* efficient, but seems to be quick enough for now
    
    * Consistently hide the cursor, for now
    
    Later we might be able to support "real" cursor rendering, but that's
    not something that Ink supports natively, so we'll have to get
    clever....

---
+++ src/cli/saya/util/ink.cljs
@@ -0,0 +1,69 @@
+(ns saya.util.ink
+  (:require
+   ["ansi-escapes" :as ansi]
+   [applied-science.js-interop :as j]
+   [clojure.string :as str]))
+
+(defn update-screen [{:keys [out last-lines]
+                      :as state}
+                     output]
+  (let [lines (str/split-lines output)]
+    (if-not (= (count lines)
+               (count last-lines))
+      ; Either this is the first render, of the lines count
+      ; has changed (perhaps due to a resize). Just start
+      ; from scratch:
+      (do
+        (.write out ansi/clearTerminal)
+        (.write out output))
+
+      ; Diff each line
+      (doseq [i (range (count lines))]
+        (let [last (nth last-lines i)
+              this (nth lines i)]
+          (when-not (= last this)
+            (.write out (ansi/cursorTo 0 i))
+            (.write out ansi/eraseLine)
+            (.write out this)))))
+
+    ; TODO: Real cursor support?
+    (.write out ansi/cursorHide)
+
+    (-> state
+        (update :history (fnil conj []) lines)
+        (assoc
+         :last-lines lines
+         :last-output output))))
+
+(defn- strip-provided-ansi [output]
+  ; Let update-screen work with a clean slate. Since we're
+  ; always a "full screen" app, output *should always* start
+  ; with clearTerminal. Occasionally, however, ink tries
+  ; to render an incomplete frame or something. We ignore those,
+  ; since they typically do things like "clear N lines" which
+  ; will break things unexpectedly, and *should* be followed by
+  ; a normal "full screen" render.
+  (when (str/starts-with? output ansi/clearTerminal)
+    (subs output (count ansi/clearTerminal))))
+
+(defonce ^:private last-state (atom nil))
+
+(defn stdout []
+  (let [out js/process.stdout
+        state (atom {:out out})]
+
+    (reset! last-state state)
+
+    (js/Object.defineProperties
+     (j/obj .-write (partial swap! state
+                             (fn [state str]
+                               (if-let [str (strip-provided-ansi str)]
+                                 (update-screen state str)
+                                 state)))
+            .-on (.bind (.-on out) out)
+            .-off (.bind (.-off out) out))
+     #js {:rows #js {:get #(.-rows out)}
+          :columns #js {:get #(.-columns out)}})))
+
+(comment
+  (map count (:history @@last-state)))
