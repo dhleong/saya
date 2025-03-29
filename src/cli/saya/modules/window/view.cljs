446c1d7673e1e9624305d26f20f979ecf9d1e5b4
Daniel Leong <me@dhleong.net>
2025-03-28 20:28:59 -0400

    Render the pending input buffer "dimmed" when not focused
    This gives us a nice reminder that something is there!
    
    Actually making this was kind of a pain. I'm not entirely sure why, but
    if we have the "suffix-text" component in a separate k/Box from the
    k/Text containing the cursor, 3 chars get truncated from the end of the
    k/Box holding the buffer-line text. Very strange!
    
    It's not *terribly* surprising given how much we're abusing ink here,
    but... still annoying.

--- src/cli/saya/modules/window/view.cljs
+++ src/cli/saya/modules/window/view.cljs
@@ -49,7 +49,9 @@
                  (>evt [:connection/send {:connr connr
                                           :text text}]))}])
 
-(defn- buffer-line [{:keys [line col]} {:keys [cursor-col input-connr]}]
+(defn- buffer-line [{:keys [cursor-col input-line? suffix-text]
+                     {:keys [line col]} :line}
+                    & children]
   (let [cursor-type (case (<sub [:mode])
                       :insert :pipe
                       :operator-pending :underscore
@@ -58,31 +60,41 @@
         ; we have a cursor to render there
         line (or (seq line)
                  [""])]
-    [:> k/Box {:min-height 0
-               :width :100%
-               :flex-wrap (when input-connr :wrap)
-               :align-items :left}
-     [:> k/Box {:height 1
-                :flex-shrink 0
-                :flex-direction :column
-                :width (when-not input-connr
-                         :100%)}
-      [:> k/Text {:wrap :truncate-end}
-       (for [[i part] (map-indexed vector (or (seq line)
-                                              [""]))]
-         ^{:key i}
-         [:<>
-          (when (= cursor-col (+ col i))
-            [cursor cursor-type])
-          (if (vector? part)
-            (into [(system-messages (first part))] (rest part))
-            [:> k/Text part])])
+    (into
+     [:> k/Box {:min-height 0
+                :width :100%
+                :flex-wrap (when input-line? :wrap)
+                :align-items :left}
+      [:> k/Box {:height 1
+                 :flex-shrink 0
+                 :flex-direction :column
+                 ; :width (cond
+                 ;          (and cursor-col input-line?) 48
+                 ;          (not input-line?) :100%)
+                 :width (when-not input-line?
+                          :100%)}
+       [:> k/Text {:wrap :truncate-end}
+        (for [[i part] (map-indexed vector line)]
+          ^{:key i}
+          [:<>
+           (when (= cursor-col (+ col i))
+             [cursor cursor-type])
+           (if (vector? part)
+             (into [(system-messages (first part))] (rest part))
+             [:> k/Text part])])
 
-       ; cursor at eol
-       (when (= cursor-col (count line))
-         [cursor cursor-type])]]
-     (when input-connr
-       [input-window input-connr])]))
+         ; cursor at eol
+        (when (= cursor-col (count line))
+          [cursor cursor-type])
+
+        suffix-text]]]
+     children)))
+
+(defn- input-placeholder [input-connr]
+  (when-some [text (<sub [::subs/input-text input-connr])]
+    [:> k/Text {:dim-color true
+                :wrap :truncate-end}
+     text]))
 
 (defn window-view [id]
   (let [ref (React/useRef)]
@@ -101,8 +113,8 @@
         (let [focused? (<sub [::subs/focused? id])
               {cursor-row :row cursor-col :col} (when focused?
                                                   (<sub [::buffer-subs/buffer-cursor bufnr]))
-              input-connr (when (<sub [::subs/input-focused? id])
-                            (<sub [::buffer-subs/->connr bufnr]))
+              input-focused? (<sub [::subs/input-focused? id])
+              input-connr (<sub [::buffer-subs/->connr bufnr])
               last-row (:row (last lines))
               scrolled? (<sub [::subs/scrolled? id])]
           [:> k/Box {:flex-direction :column
@@ -113,17 +125,43 @@
                       :flex-grow 1
                       :width :100%}
             (for [{:keys [row col line last-of-row?] :as data} lines]
-              ^{:key [id row col]}
-              [buffer-line
-               data
-               {:cursor-col (when (and (= cursor-row row)
-                                       (or last-of-row?
-                                           (<= col cursor-col (dec (+ col (count line))))))
-                              cursor-col)
-                :input-connr (when (and (= last-row row)
-                                        (not scrolled?))
-                               input-connr)}])]
-           (when scrolled?
-             (if input-connr
-               [input-window (<sub [::buffer-subs/->connr bufnr])]
-               [placeholders/line]))])))))
+              (let [input-line? (and (= last-row row)
+                                     (not scrolled?)
+                                     (some? input-connr))
+                    inputting? (and input-line? input-focused?)]
+                ^{:key [id row col]}
+                [buffer-line
+                 {:line data
+                  :input-line? input-line?
+                  :cursor-col (when (and (not inputting?)
+                                         (= cursor-row row)
+                                         (or last-of-row?
+                                             (<= col cursor-col
+                                                 (dec (+ col (count line))))))
+                                cursor-col)
+
+                  ; NOTE: It'd sure be nice if we could just pass this as a
+                  ; child *but* for some reason when the cursor is on the line,
+                  ; unless this extra text is part of the same k/Text tree, 3
+                  ; chars get truncated from the buffer-line part... It's
+                  ; definitely something to do with our cursor hack but I'm not
+                  ; certain *what*, exactly.
+                  :suffix-text (when (and input-line?
+                                          (not inputting?))
+                                 [input-placeholder input-connr])}
+                 (when (and input-line? inputting?)
+                   [input-window input-connr])]))]
+           (cond
+             (and scrolled? input-focused? input-connr)
+             [input-window (<sub [::buffer-subs/->connr bufnr])]
+
+             (and scrolled? input-connr)
+             [input-placeholder input-connr]
+
+             scrolled?
+             [placeholders/line]
+
+             ; NOTE: We *may* actually want to render something here to avoid the
+             ; window size changing when we scroll... For now, though...
+             ; it looks nicer without anything!
+             :else nil)])))))
