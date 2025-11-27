763b0103c0035955f26b8472890d0b32f98cafe3
Daniel Leong <me@dhleong.net>
2025-11-26 23:15:46 -0500

    Try stubbing out :stdin to see if that resolves CI issues

--- src/test/saya/util/ink_testing_utils.cljs
+++ src/test/saya/util/ink_testing_utils.cljs
@@ -9,6 +9,30 @@
    [saya.reagent :as reagent]
    [saya.util.ink :as ink]))
 
+(defn- create-fake-stdout [last-frame width height]
+  (let [events (EventEmitter.)]
+    (js/Object.defineProperties
+     (j/obj .-write (fn [f]
+                      (swap! last-frame str f))
+            .-on (.bind (.-on events) events)
+            .-off (.bind (.-off events) events))
+     #js {:rows #js {:get (constantly height)}
+          :columns #js {:get (constantly width)}})))
+
+(defn- create-fake-stdin []
+  (let [events (EventEmitter.)]
+    (js/Object.defineProperties
+     (j/obj .-isTTY true
+            .-setRawMode (fn [_] nil)
+            .-resume (fn [] nil)
+            .-pause (fn [] nil)
+            .-ref (fn [] nil)
+            .-unref (fn [] nil)
+            .-on (.bind (.-on events) events)
+            .-off (.bind (.-off events) events)
+            .-emit (.bind (.-emit events) events))
+     #js {})))
+
 (defn render->string
   ([component] (render->string {} component))
   ([{:keys [width height ansi? position-cursor?]
@@ -18,14 +42,8 @@
     component]
    (>evt [:saya.events/set-dimens width height])
    (let [last-frame (atom "")
-         events (EventEmitter.)
-         stdout (js/Object.defineProperties
-                 (j/obj .-write (fn [f]
-                                  (swap! last-frame str f))
-                        .-on (.bind (.-on events) events)
-                        .-off (.bind (.-off events) events))
-                 #js {:rows #js {:get (constantly height)}
-                      :columns #js {:get (constantly width)}})
+         stdout (create-fake-stdout last-frame width height)
+         stdin (create-fake-stdin)
          ink-state (atom {:out stdout
                           :cursor-shape? false
                           :position-cursor? position-cursor?})
@@ -33,6 +51,7 @@
                         #js {:stdout (ink/stdout
                                       {:always-render? true}
                                       ink-state stdout)
+                             :stdin stdin
                              :debug true
                              :extOnCtrlC false
                              :patchConsole false})]
