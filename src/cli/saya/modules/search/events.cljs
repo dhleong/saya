4a21cab20ad8de394967b8228aa0af745c47700b
Daniel Leong <me@dhleong.net>
2025-12-01 12:38:10 -0500

    Fix: not preparing search cmdline-window buffer

--- src/cli/saya/modules/search/events.cljs
+++ src/cli/saya/modules/search/events.cljs
@@ -1,6 +1,20 @@
 (ns saya.modules.search.events
   (:require
-   [re-frame.core :refer [reg-event-db trim-v]]))
+   [re-frame.core :refer [reg-event-db trim-v]]
+   [saya.modules.buffers.line :refer [buffer-line]]))
+
+(reg-event-db
+ ::prepare-buffer
+ [trim-v]
+ (fn [db [pending-input-line]]
+   (assoc-in
+    db
+    [:buffers :search]
+    {:lines (-> (get-in db [:histories :search])
+                (->> (map buffer-line))
+                (reverse)
+                (vec)
+                (conj (buffer-line pending-input-line)))})))
 
 (reg-event-db
  ::submit
