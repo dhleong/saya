4e7fb48a93cfed1d014d0b12d30dc65c62bb6a83
Daniel Leong <me@dhleong.net>
2025-12-11 13:51:46 -0500

    Handle invalid keymaps gracefully instead of crashing

--- src/cli/saya/modules/scripting/events.cljs
+++ src/cli/saya/modules/scripting/events.cljs
@@ -1,15 +1,26 @@
 (ns saya.modules.scripting.events
   (:require
-   [re-frame.core :refer [reg-event-db unwrap]]
+   [re-frame.core :refer [reg-event-fx unwrap]]
+   [saya.modules.echo.core :refer [echo-fx]]
    [saya.modules.scripting.config :refer [format-user-keymaps]]))
 
-(reg-event-db
+(reg-event-fx
  ::reconfigure-connection
  [unwrap]
- (fn [db {:keys [connection-id script-file keymaps]}]
-   (let [bufnr (get-in db [:connections connection-id :bufnr])]
-     (-> db
-         (assoc-in [:connections connection-id :script-file] script-file)
-         (assoc-in [:buffers bufnr :keymaps] (format-user-keymaps
-                                              connection-id
-                                              keymaps))))))
+ (fn [{:keys [db]} {:keys [connection-id script-file keymaps]}]
+   (let [bufnr (get-in db [:connections connection-id :bufnr])
+         [keymaps err] (try
+                         [(format-user-keymaps
+                           connection-id
+                           keymaps)
+                          nil]
+                         (catch :default e
+                           [nil e]))]
+     {:db (cond-> db
+            :always
+            (assoc-in [:connections connection-id :script-file] script-file)
+
+            keymaps
+            (assoc-in [:buffers bufnr :keymaps] keymaps))
+      :fx [(when err
+             (echo-fx :error "Error parsing keymaps: " err))]})))
