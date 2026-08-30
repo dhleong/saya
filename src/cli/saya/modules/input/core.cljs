5e6eb7403da3f012546855c5e5ee4ad591dbfd9b
Daniel Leong <me@dhleong.net>
2026-08-30 17:31:26 -0400

    Fix unused import

--- src/cli/saya/modules/input/core.cljs
+++ src/cli/saya/modules/input/core.cljs
@@ -8,7 +8,6 @@
    [saya.modules.command.interceptors :refer [with-buffer-context]]
    [saya.modules.connection.events :as conn-events]
    [saya.modules.echo.events :as echo-events]
-   [saya.modules.input.events :as input-events]
    [saya.modules.input.fx :as fx]
    [saya.modules.input.helpers :refer [update-cursor]]
    [saya.modules.input.insert :as insert]
